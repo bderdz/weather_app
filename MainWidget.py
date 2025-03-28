2e215a2045c8f5d094672572fc69e19628e5276b
Bogdan Derdziak <bogdan.derdz@gmail.com>
2025-03-28 11:34:53 +0100

    weather api connected

--- MainWidget.py
+++ MainWidget.py
@@ -1,12 +1,12 @@
 from PySide6.QtWidgets import QWidget, QLineEdit, QPushButton, QLabel, QGridLayout
-
+import requests
 
 class MainWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowTitle("Weather")
 
-        self.city_edit = QLineEdit(self)
+        self.city_edit = QLineEdit("Lublin", self)
         get_cities_btn = QPushButton("Get Cities", self)
         self.weather_label = QLabel(self)
         get_cities_btn.clicked.connect(self.get_cities)
@@ -17,4 +17,17 @@ class MainWidget(QWidget):
         layout.addWidget(self.weather_label, 1, 0, 1, 2)
 
     def get_cities(self):
-        self.weather_label.setText(self.city_edit.text())
+        # self.weather_label.setText(self.city_edit.text())
+        url = f"https://geocoding-api.open-meteo.com/v1/search?name={self.city_edit.text()}"
+        response = requests.get(url)
+        json = response.json()
+
+        if "results" not in json.keys():
+            print("No such city")
+            return
+
+        results = json["results"]
+        city = results[0]
+        latitude = city["latitude"]
+        longitude = city["longitude"]
+        print(latitude, longitude)
